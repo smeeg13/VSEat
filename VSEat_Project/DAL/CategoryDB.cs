c3ab975e4a40165d8a97def3c9d4060f71f9f182
etdnt <emilie.teodorodonascimento@student.hevs.ch>
2021-10-18 17:28:30 +0200

    Category and Deliverer modifs

--- VSEat_Project/DAL/CategoryDB.cs
+++ VSEat_Project/DAL/CategoryDB.cs
@@ -1,13 +1,106 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    class CategoryDB
+    public class CategoryDB 
     {
+        private IConfiguration Configuration { get; }
 
+        public CategoryDB(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public List<Category> GetCategory()
+        {
+            List<Category> results = null;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "Select * from Category";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+
+                    cn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            if (results == null)
+                                results = new List<Category>();
+
+                            Category category = new Category();
+
+                            category.IdCategory = (int)dr["IdCategory"];
+
+                            if (dr["NameCategory"] != null)
+                                category.NameCategory = (string)dr["NameCategory"];
+
+                            if (dr["DescriptionCategory"] != null)
+                                category.DescriptionCategory = (string)dr["DescriptionCategory"];
+
+                            results.Add(category);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return results;
+        }
+
+        public Category GetCategory(string nameCategory, string descriptionCategory)
+        {
+            Category category = null;
+
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "Select * from Category where NameCategory = @NameCategory AND DescriptionCategory = @DescriptionCategory";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@NameCategory", nameCategory);
+                    cmd.Parameters.AddWithValue("@DescriptionCategory", descriptionCategory);
+
+                    cn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            category = new Category();
+
+                            category.IdCategory = (int)dr["idCategory"];
+
+                            if(dr["NameCategory"] != null)
+                                category.NameCategory = (string)dr["NameCategory"];
+
+                            if (dr["DescriptionCategory"] != null)
+                                category.DescriptionCategory = (string)dr["DescriptionCategory"];
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return category;
+        }
     }
 }
---
+++ VSEat_Project/DAL/IDeliverer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    interface IDeliverer
+    {
+        List<Deliverer> GetDeliverers();
+        Order GetDeliverer(string email, string password);
+        void AddDeliverer(Deliverer deliverer);
+        void DeleteDeliverer(int id);
+    }
+}
