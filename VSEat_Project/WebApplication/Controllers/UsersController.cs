3fd4c3c24b83c30ab70dbef65fd0f9d13c774f76
megane <megane.solliard@students.hevs.ch>
2021-11-30 15:42:32 +0100

    add view

---
+++ VSEat_Project/WebApplication/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using BLL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IUserManager UserManager { get; }
+
+
+        public UsersController(IUserManager usermanager)
+        {
+            UserManager = usermanager;
+        }
+
+        //GET :UsersController
+        public IActionResult Index()
+        {
+            var users = UserManager.GetUsers();
+            return View(users);
+        }
+    }
+}
